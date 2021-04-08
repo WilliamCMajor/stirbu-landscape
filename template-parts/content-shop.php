28d4f8ca18a8f30ab9b9c6d578c852f521573548
WilliamCMajor <she8@nait.ca>
2021-04-08 12:46:28 -0600

    shop product

--- template-parts/content-shop.php
+++ template-parts/content-shop.php
@@ -10,8 +10,25 @@ get_header();
 ?>
 
 <main id="main" class="site-main" role="main">
-    <?php echo do_shortcode ('[products limit="6" columns="2"]'); ?> 
-    <h1>asdkjasldkjasd</h1>   
+    <div class="shop-category">
+        <ul>
+            <li><a href="">shop</a></li>
+            <li><a href="">best sellers</a></li>
+            <li><a href="">mountain</a></li>
+            <li><a href="">urban</a></li>
+            <li><a href="">nature</a></li>
+            <li><a href="">landscapes</a></li>
+        </ul>
+    </div>
+    <div class="shop-main">
+        <div class="filter">
+            <h2>Filter By:</h2>
+            <?php dynamic_sidebar('sidebar'); ?>
+        </div>
+        <div class="shop-container">
+            <?php echo do_shortcode ('[products]'); ?> 
+        </div>
+    </div>
 </main>
 
 
